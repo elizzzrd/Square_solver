42a1709f3ceda0759060f565e11ba47a4aad15c9
Elizaveta Gardina <ligigard@gmail.com>
2025-08-22 14:14:59 +0300

    добавлено разделение на файлы и функция is_zero()

---
+++ second-version/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <stdbool.h>
+#include "ss.h"
+
+
+
+int main(void)
+{
+	struct input Coefs;
+	struct output Solution;
+
+	printf("Solving ax^2 + bx + c = 0\n");
+	get_input(&Coefs);
+
+	printf("You've entered:\n");
+	printf("a = %.2lf, b = %.2lf, c = %.2lf\n", Coefs.a, Coefs.b, Coefs.c);
+
+	if (is_zero(Coefs.a))
+	{
+		linear_equation_solve(&Coefs, &Solution);
+	}
+	else
+	{
+		square_equation_solve(&Coefs, &Solution);
+	}
+
+	show_solution(&Solution);
+
+	printf("The program is finished.\n");
+    return 0;
+}
